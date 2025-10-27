3c7ceb8ddae9396dfe00f9b258125514a9d0a288
Zackarias Häger <101270852+Zacke1234@users.noreply.github.com>
2025-10-27 17:11:22 +0100

    push!!!!

--- Managers/ObjectManager.cpp
+++ Managers/ObjectManager.cpp
@@ -1,13 +1,12 @@
 #include "ObjectManager.h"
 
-Object* ObjectManager::Create(std::string _namn = "new_object", Mesh* Mesh = NULL, Texture* aTexture = NULL, Shader* aShader = NULL, Collider* aCollider = NULL)
+Object* ObjectManager::Create(std::string _namn = "new_object", Mesh* Mesh = NULL, Texture* aTexture = NULL, Shader* aShader = NULL, Collider* aCollider = NULL) 
 {
 	Object* obj = new Object(_namn, Mesh, aTexture, aShader, aCollider);
 	Object::Entities.push_back(obj);
 	return obj;
 }
 
-void ObjectManager::Destroy(Object* obj)
-{
-	
+void ObjectManager::Destroy(Object* obj) {
+	Destroy(obj);
 }
